55fee898b33658e6b39a906ae635986e17c9b09e
alexander <alexanderhardeland@gmail.com>
2024-04-17 12:46:19 +0200

    Installed cypress and jest but cant get eslint to work with them

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,16 @@
+import js from '@eslint/js'
+import jest from 'eslint-plugin-jest'
+import cypress from 'eslint-plugin-cypress'
+
 export default [
+  js.configs.recommended,
   {
+    plugins: {
+      cypress,
+      jest,
+    },
     rules: {
-      'no-unused-vars': 'error',
+      'no-unused-vars': 'warn',
       'no-undef': 'error',
     },
   },
